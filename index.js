0a02387e09cc3683559d8c600b29da31005117e5
Nikkana <fakehere253@gmail.com>
2023-11-19 16:48:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Hey it's me your fav Kiran xer Here');
+    res.write('Hey its me your fav Kiran xer Here');
     res.end();
 }).listen(process.env.PORT || 3000);
